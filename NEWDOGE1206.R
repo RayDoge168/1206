f686e8d5a3c90d25820996c5246a51ecb3e86cee
ray1688 <shih882101@gmail.com>
2023-12-06 16:41:28 +0800

    臺大醫院 自訂函數台大急診即時資訊

--- NEWDOGE1206.R
+++ NEWDOGE1206.R
@@ -12,3 +12,57 @@ end.TITLE.pos = start.TITLE.pos + attr(TITLE.pos[[1]], "match.length")[1] - 1
 TITLE.word = substr(txt_new, start.TITLE.pos, end.TITLE.pos)
 
 TITLE.word
+
+TITLE.word = gsub("<title>", "", TITLE.word)
+TITLE.word = gsub("</title>", "", TITLE.word)
+TITLE.word
+
+start.pos = gregexpr("<tr>", txt_new)
+end.pos = gregexpr("</tr>", txt_new)
+
+i = 1
+sub.start.pos = start.pos[[1]][i]
+sub.end.pos = end.pos[[1]][i] + attr(end.pos[[1]], "match.length")[i] - 1
+
+sub_txt = substr(txt_new, sub.start.pos, sub.end.pos)
+sub_txt
+
+sub_txt = gsub('单本腹计', '', sub_txt)
+sub_txt = gsub('</?tr>', '', sub_txt)
+sub_txt = gsub('</?td>', '', sub_txt)
+sub_txt = gsub(' ', '', sub_txt)
+sub_txt
+
+NTU_info = function () {
+  
+  result = data.frame(item = c('单本腹计', '单禘计', '单皘计', '单ICU计', '单崩计'),
+                      info = NA,
+                      stringsAsFactors = FALSE)
+  
+  URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
+  
+  txt = scan(URL, what = "character", encoding = "UTF-8", quiet = TRUE)
+  txt_new = paste(txt, sep = "", collapse = " ")
+  
+  start.pos = gregexpr("<tr>", txt_new)
+  end.pos = gregexpr("</tr>", txt_new)
+  
+  for (i in 1:5) {
+    
+    sub.start.pos = start.pos[[1]][i]
+    sub.end.pos = end.pos[[1]][i] + attr(end.pos[[1]], "match.length")[i] - 1
+    
+    sub_txt = substr(txt_new, sub.start.pos, sub.end.pos)
+    sub_txt = gsub('单.*', '', sub_txt)
+    sub_txt = gsub('</?tr>', '', sub_txt)
+    sub_txt = gsub('</?td>', '', sub_txt)
+    result[i,'info'] = gsub(' ', '', sub_txt)
+    
+  }
+  
+  result
+  
+}
+
+NTU_info()
+
